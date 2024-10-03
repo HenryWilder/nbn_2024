c56b63719b5d409f00beb4256d9a158cb9fdcef9
Henry Wilder <henrythepony@gmail.com>
2024-10-02 23:14:34 -0400

    Rearranged

--- scripts/Grenades/Customizer.cs
+++ scripts/Grenades/Customizer.cs
@@ -4,12 +4,10 @@ using Godot;
 public partial class Customizer : GrenadeBase
 {
     private readonly Device device = new();
-    private ulong startTime = ulong.MaxValue;
+    private ulong startTime = Time.GetTicksMsec();
 
     public override void _Ready()
     {
-        startTime = Time.GetTicksMsec();
-
         // todo: have user insert a ROM manually
         Insert(ROM.ExampleROM);
     }
@@ -17,6 +15,8 @@ public partial class Customizer : GrenadeBase
     public override void _Process(double delta)
     {
         device.cpu.reg.Tms = (short)(Time.GetTicksMsec() - startTime);
+        const float MILLI = 1.0f / 1000.0f;
+        device.cpu.reg.Vel = (short)(LinearVelocity.Length() * MILLI);
         try {
             device.Step();
         } catch (Exception e) {
@@ -26,13 +26,6 @@ public partial class Customizer : GrenadeBase
         if (device.cpu.reg.Bam != 0) {
             Explode();
         }
-    }
-
-    public override void _PhysicsProcess(double delta)
-    {
-        const float MILLI = 1.0f / 1000.0f;
-        base._PhysicsProcess(delta);
-        device.cpu.reg.Vel = (short)(LinearVelocity.Length() * MILLI);
     }
 
     protected override void OnHit(Node other)
